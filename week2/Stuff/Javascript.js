b6e29a2ecf0fa09b2610cafd0fdc2cbd530a8e47
Jaedon-nanohackers <Jsballeyne@optonline.net>
2017-03-17 18:47:24 -0400

    Stuff that I forgot to commit
    contains class work and homework

---
+++ week2/Stuff/Javascript.js
@@ -0,0 +1,12 @@
+//steak porkchop rice bread chicken
+let food = {
+  chicken:{
+    calories: 250,
+    ingrideants:["goya", "pepper", "salt"]
+  },
+  rice:{
+    calories: 86,
+    type:["brown","white", "fried"]
+  }
+
+}
